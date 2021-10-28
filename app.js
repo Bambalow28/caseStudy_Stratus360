09924bf0e9ff9df3aa60d060018eff88a41b8628
Joshua Alanis <joshalanis28@gmail.com>
2021-10-28 02:42:02 -0400

    UI Fixes

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-const router = express.Router();
-const cors = require("cors")
+const cors = require('cors');
 
-app.use(cors({
-    origin: 'https://case-study-stratus-360.herokuapp.com/',
-    }));
+const app = express();
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(process.env.port || 8080);
